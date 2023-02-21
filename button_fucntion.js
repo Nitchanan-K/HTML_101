ad0a9f03c363d4ee9bf4fb7f32ed431e87ee278c
nitchanan.k <nitchanan_katinasatian@hotmail.com>
2023-02-22 00:47:24 +0700

    add hover

---
+++ button_fucntion.js
@@ -0,0 +1,18 @@
+const button = document.getElementById('btn');
+
+button.addEventListener('mouseover', function (){
+    button.style.left = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+})
+button.addEventListener('click', function () {
+    alert('you clicked me')
+}) 
+
+/*
+emailInput.addEventListener("input", function() {
+    if (emailInput.value !== '') {
+        button.style.position = fixed;
+    }
+    });
+
+    /*
